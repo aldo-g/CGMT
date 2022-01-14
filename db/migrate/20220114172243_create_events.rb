d1c992d235ad57203fd22fe10be1731d89118b93
Alastair Grant <alastairegrant@gmail.com>
2022-01-14 17:23:07 +0000

    event model created

---
+++ db/migrate/20220114172243_create_events.rb
@@ -0,0 +1,16 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :organizer
+      t.date :date
+      t.string :desription
+      t.string :image
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
