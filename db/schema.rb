1354dfc9defbe0a9a3c63458b3a22d62a49835b0
Alastair Grant <alastairegrant@gmail.com>
2022-03-16 12:38:42 +0000

    password added to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_140125) do
+ActiveRecord::Schema.define(version: 2022_03_16_123659) do
 
   create_table "applications", force: :cascade do |t|
     t.string "type"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2022_03_03_140125) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
 end
