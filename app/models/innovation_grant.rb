db1b35d741f766922eebccea560550456899e629
Alastair Grant <alastairegrant@gmail.com>
2022-03-16 12:32:38 +0000

    validations added to innovation grant model

--- app/models/innovation_grant.rb
+++ app/models/innovation_grant.rb
@@ -1,2 +1,5 @@
 class InnovationGrant < ApplicationRecord
+  validates :project_name, presence:true
+  validates :organisation_name, presence:true
+  validates :project_description, presence:true
 end
