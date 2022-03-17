d2489090a7b687c5bc37fb40aa730b25980f8078
Alastair Grant <alastairegrant@gmail.com>
2022-03-17 11:53:43 +0000

    removed name from user

--- db/schema.rb
+++ db/schema.rb
@@ -76,7 +76,6 @@ ActiveRecord::Schema.define(version: 2022_03_16_123659) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
