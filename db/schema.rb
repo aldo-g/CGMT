e66b2e3ae28a8245846491875f612fbf5b4e782f
Alastair Grant <alastairegrant@gmail.com>
2022-03-17 12:37:06 +0000

    user model destroyed

--- db/schema.rb
+++ db/schema.rb
@@ -75,11 +75,4 @@ ActiveRecord::Schema.define(version: 2022_03_16_123659) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-  end
-
 end
