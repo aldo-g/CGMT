8c89950a59f514ecf303c459020402e48cae0c96
Alastair Grant <alastairegrant@gmail.com>
2022-03-17 11:51:24 +0000

    password changed to password digest

--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 2022_03_16_123659) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
