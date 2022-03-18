23bdd943e51ff9211572a8e4f1c161c2b0e125ed
Alastair Grant <alastairegrant@gmail.com>
2022-03-18 13:58:16 +0000

    email formatting validation added 2

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,6 @@
 class Application < ApplicationRecord
   validates :type, presence:true
-  validates :email, presence:true
+  validates :email, presence:true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
   validates :name, presence:true
   validates :organisation, presence:true
   validates :text, presence:true
