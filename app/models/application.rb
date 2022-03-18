cdcc2f896385eec1f578bacbbab8f952e1fb2187
Alastair Grant <alastairegrant@gmail.com>
2022-03-18 14:04:42 +0000

    further validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,6 @@
 class Application < ApplicationRecord
   validates :type, presence:true
-  validates :email, presence:true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  validates :email, presence:true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, message: "must be a valid email address"
   validates :name, presence:true
   validates :organisation, presence:true
   validates :text, presence:true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, presence:true
-  validates :email, presence:true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  validates :email, presence:true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, message: "must be a valid email address"
   validates :password_digest, presence:true
 end
