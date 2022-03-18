d740f6757baffad449c984cf48e2e26d6a502433
Alastair Grant <alastairegrant@gmail.com>
2022-03-18 13:56:43 +0000

    email formatting validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, presence:true
-  validates :email, presence:true
+  validates :email, presence:true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
   validates :password_digest, presence:true
 end
