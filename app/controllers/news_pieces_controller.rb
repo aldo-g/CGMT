e6b2bdc03752958b699c4a744677869e5cbfc94c
Alastair Grant <alastairegrant@gmail.com>
2022-03-19 00:52:22 +0000

    small news piece controller methods added

--- app/controllers/news_pieces_controller.rb
+++ app/controllers/news_pieces_controller.rb
@@ -1,2 +1,21 @@
 class NewsPiecesController < ApplicationController
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+  def new
+    @restaurant = Restaurant.new # needed to instantiate the form_for
+  end
+  def create
+    @restaurant = Restaurant.new(params[:restaurant])
+    @restaurant.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to restaurant_path(@restaurant)
+  end
+  
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :rating, :address)
+  end
+
 end
