1e660fc977b9018cbede7a2be650063fd88891c4
Alastair Grant <alastairegrant@gmail.com>
2022-03-31 19:40:40 +0100

    user ref added to partners

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_30_151234) do
+ActiveRecord::Schema.define(version: 2022_03_31_184009) do
 
   create_table "applications", force: :cascade do |t|
     t.string "type"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 2022_03_30_151234) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_partners_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -83,4 +85,5 @@ ActiveRecord::Schema.define(version: 2022_03_30_151234) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "partners", "users"
 end
