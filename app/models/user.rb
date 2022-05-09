9fc97966dc113748f8baf508767c0194a66d8666
Alastair Grant <alastairegrant@gmail.com>
2022-05-09 11:25:03 +0100

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, presence:true
-  validates :email, presence:true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, message: "must be a valid email address"
   validates :password_digest, presence:true
   has_many :partners
   has_many :news_pieces
   has_many :innovation_grants
   has_many :events
   has_many :coasters
-  
+
 end
